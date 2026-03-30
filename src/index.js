16fc469999b16421d71d5d82435645242bfcfac6
mooncrown04 <mooncrown04@gmail.com>
2026-03-30 19:26:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,134 +1,128 @@
-import http from 'http'
-import Stremio from 'stremio-addons'
-import serveStatic from 'serve-static'
-import chalk from 'chalk'
-import pkg from '../package.json'
-import PornClient from './PornClient'
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
 
-const SUPPORTED_METHODS = [
-  'stream.find', 'meta.find', 'meta.search', 'meta.get',
-]
-const STATIC_DIR = 'static'
-const DEFAULT_ID = 'stremio_porn'
+var _http = _interopRequireDefault(require("http"));
+var _stremioAddons = _interopRequireDefault(require("stremio-addons"));
+var _serveStatic = _interopRequireDefault(require("serve-static"));
+var _chalk = _interopRequireDefault(require("chalk"));
+var _package = _interopRequireDefault(require("../package.json"));
+var _PornClient = _interopRequireDefault(require("./PornClient"));
 
-const ID = process.env.STREMIO_PORN_ID || DEFAULT_ID
-const ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'http://localhost'
-const PORT = process.env.STREMIO_PORN_PORT || process.env.PORT || '80'
-const PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY
-const CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1'
-const EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL
-const IS_PROD = process.env.NODE_ENV === 'production'
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } } function _next(value) { step("next", value); } function _throw(err) { step("throw", err); } _next(); }); }; }
 
-if (IS_PROD && ID === DEFAULT_ID) {
-  // eslint-disable-next-line no-console
-  console.error(
-    chalk.red(
-      '\nWhen running in production, a non-default addon identifier must be specified\n'
-    )
-  )
-  process.exit(1)
-}
+// Desteklenen metodlara catalog.find eklendi
+const SUPPORTED_METHODS = ['stream.find', 'meta.find', 'meta.search', 'meta.get', 'catalog.find'];
+const STATIC_DIR = 'static';
+const DEFAULT_ID = 'stremio_porn';
+const ID = process.env.STREMIO_PORN_ID || DEFAULT_ID;
+const ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'http://localhost';
+const PORT = process.env.STREMIO_PORN_PORT || process.env.PORT || '80';
+const PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY;
+const CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1';
+const EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL;
+const IS_PROD = process.env.NODE_ENV === 'production';
 
-let availableSites = PornClient.ADAPTERS.map((a) => a.DISPLAY_NAME).join(', ')
+// Katalogları otomatik oluştur (Her adaptör için bir menü başlığı)
+const CATALOGS = _PornClient.default.ADAPTERS.map(adapter => ({
+  type: 'movie',
+  id: `catalog_${adapter.ID}`,
+  name: adapter.DISPLAY_NAME,
+  extra: [{ name: 'search' }, { name: 'skip' }]
+}));
 
 const MANIFEST = {
-  name: 'Porn',
+  name: 'Porn+',
   id: ID,
-  version: pkg.version,
-  description: `\
-Time to unsheathe your sword! \
-Watch porn videos and webcam streams from ${availableSites}\
-`,
+  version: _package.default.version,
+  description: `Discover movies and streams from ${_PornClient.default.ADAPTERS.map(a => a.DISPLAY_NAME).join(', ')}`,
   types: ['movie', 'tv'],
-  idProperty: PornClient.ID,
+  idProperty: _PornClient.default.ID,
   dontAnnounce: !IS_PROD,
-  sorts: PornClient.SORTS,
-  // The docs mention `contactEmail`, but the template uses `email`
+  sorts: _PornClient.default.SORTS,
   email: EMAIL,
   contactEmail: EMAIL,
   endpoint: `${ENDPOINT}/stremioget/stremio/v1`,
   logo: `${ENDPOINT}/logo.png`,
   icon: `${ENDPOINT}/logo.png`,
   background: `${ENDPOINT}/bg.jpg`,
-  // OBSOLETE: used in pre-4.0 stremio instead of idProperty/types
+  catalogs: CATALOGS, // Sinewix gibi sol menüde çıkmasını sağlayan kısım
+  resources: ['catalog', 'meta', 'stream'],
   filter: {
-    [`query.${PornClient.ID}`]: { $exists: true },
-    'query.type': { $in: ['movie', 'tv'] },
-  },
-}
-
+    [`query.${_PornClient.default.ID}`]: { $exists: true },
+    'query.type': { $in: ['movie', 'tv'] }
+  }
+};
 
 function makeMethod(client, methodName) {
-  return async (request, cb) => {
-    let response
-    let error
+  return function () {
+    var _ref = _asyncToGenerator(function* (request, cb) {
+      let response;
+      let error;
+      try {
+        response = yield client.invokeMethod(methodName, request);
+      } catch (err) {
+        error = err;
+        console.error(_chalk.default.gray(new Date().toLocaleString()) + ` Error in ${methodName}:`, err);
+      }
+      cb(error, response);
+    });
+    return function (_x, _x2) { return _ref.apply(this, arguments); };
+  }();
+}
 
-    try {
-      response = await client.invokeMethod(methodName, request)
-    } catch (err) {
-      error = err
-
-      /* eslint-disable no-console */
-      console.error(
-        // eslint-disable-next-line prefer-template
-        chalk.gray(new Date().toLocaleString()) +
-        ' An error has occurred while processing ' +
-        `the following request to ${methodName}:`
-      )
-      console.error(request)
-      console.error(err)
-      /* eslint-enable no-console */
-    }
-
-    cb(error, response)
-  }
+// Katalog isteği geldiğinde (Sol menüden birine tıklandığında) çalışacak motor
+function makeCatalogMethod(client) {
+  return function () {
+    var _refCatalog = _asyncToGenerator(function* (request, cb) {
+      try {
+        const adapterId = request.id.replace('catalog_', '');
+        const response = yield client.invokeMethod('meta.search', {
+          query: {
+            type: request.type,
+            [_PornClient.default.ID]: adapterId,
+            search: (request.extra && request.extra.search) ? request.extra.search : ''
+          },
+          skip: (request.extra && request.extra.skip) ? request.extra.skip : 0
+        });
+        cb(null, response);
+      } catch (err) {
+        cb(err);
+      }
+    });
+    return function (req, callback) { return _refCatalog.apply(this, arguments); };
+  }();
 }
 
 function makeMethods(client, methodNames) {
-  return methodNames.reduce((methods, methodName) => {
-    methods[methodName] = makeMethod(client, methodName)
-    return methods
-  }, {})
+  const baseMethods = methodNames.reduce((methods, methodName) => {
+    if (methodName !== 'catalog.find') {
+      methods[methodName] = makeMethod(client, methodName);
+    }
+    return methods;
+  }, {});
+  baseMethods['catalog.find'] = makeCatalogMethod(client);
+  return baseMethods;
 }
 
+let client = new _PornClient.default({ proxy: PROXY, cache: CACHE });
+let methods = makeMethods(client, SUPPORTED_METHODS);
+let addon = new _stremioAddons.default.Server(methods, MANIFEST);
 
-let client = new PornClient({ proxy: PROXY, cache: CACHE })
-let methods = makeMethods(client, SUPPORTED_METHODS)
-let addon = new Stremio.Server(methods, MANIFEST)
-let server = http.createServer((req, res) => {
-  serveStatic(STATIC_DIR)(req, res, () => {
-    addon.middleware(req, res, () => res.end())
-  })
-})
+let server = _http.default.createServer((req, res) => {
+  (0, _serveStatic.default)(STATIC_DIR)(req, res, () => {
+    addon.middleware(req, res, () => res.end());
+  });
+});
 
-server
-  .on('listening', () => {
-    let values = {
-      endpoint: chalk.green(MANIFEST.endpoint),
-      id: ID === DEFAULT_ID ? chalk.red(ID) : chalk.green(ID),
-      email: EMAIL ? chalk.green(EMAIL) : chalk.red('undefined'),
-      env: IS_PROD ? chalk.green('production') : chalk.green('development'),
-      proxy: PROXY ? chalk.green(PROXY) : chalk.red('off'),
-      cache: (CACHE === '0') ?
-        chalk.red('off') :
-        chalk.green(CACHE === '1' ? 'on' : CACHE),
-    }
+server.on('listening', () => {
+  console.log(_chalk.default.green(`\n ✅ Addon Ready! \n Endpoint: ${MANIFEST.endpoint}`));
+}).listen(PORT);
 
-    // eslint-disable-next-line no-console
-    console.log(`
-    ${MANIFEST.name} Addon is listening on port ${PORT}
-
-    Endpoint:    ${values.endpoint}
-    Addon Id:    ${values.id}
-    Email:       ${values.email}
-    Environment: ${values.env}
-    Proxy:       ${values.proxy}
-    Cache:       ${values.cache}
-    `)
-  })
-  .listen(PORT)
-
-
-export default server
+var _default = server;
+exports.default = _default;
