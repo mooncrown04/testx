f3267088536badc7b32d50d84037c8cc10502fbc
mooncrown04 <mooncrown04@gmail.com>
2026-03-30 19:30:54 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,147 +1,119 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = void 0;
 
 var _http = _interopRequireDefault(require("http"));
-
 var _stremioAddons = _interopRequireDefault(require("stremio-addons"));
-
 var _serveStatic = _interopRequireDefault(require("serve-static"));
-
 var _chalk = _interopRequireDefault(require("chalk"));
-
 var _package = _interopRequireDefault(require("../package.json"));
-
 var _PornClient = _interopRequireDefault(require("./PornClient"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } } function _next(value) { step("next", value); } function _throw(err) { step("throw", err); } _next(); }); }; }
 
-const SUPPORTED_METHODS = ['stream.find', 'meta.find', 'meta.search', 'meta.get'];
-const STATIC_DIR = 'static';
+// Metodlara catalog.find eklendi (Nuvio uyumu için şart)
+const SUPPORTED_METHODS = ['stream.find', 'meta.find', 'meta.search', 'meta.get', 'catalog.find'];
 const DEFAULT_ID = 'stremio_porn';
 const ID = process.env.STREMIO_PORN_ID || DEFAULT_ID;
 const ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'http://localhost';
 const PORT = process.env.STREMIO_PORN_PORT || process.env.PORT || '80';
 const PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY;
 const CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1';
-const EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL;
-const IS_PROD = process.env.NODE_ENV === 'production';
 
-if (IS_PROD && ID === DEFAULT_ID) {
-  // eslint-disable-next-line no-console
-  console.error(_chalk.default.red('\nWhen running in production, a non-default addon identifier must be specified\n'));
-  process.exit(1);
-}
-
-let availableSites = _PornClient.default.ADAPTERS.map(a => a.DISPLAY_NAME).join(', ');
+// Sinewix'teki gibi dinamik katalog listesi oluşturma
+const CATALOGS = _PornClient.default.ADAPTERS.map(adapter => ({
+  type: 'movie',
+  id: `catalog_${adapter.ID}`,
+  name: `${adapter.DISPLAY_NAME} Popular`,
+  extra: [{ name: 'search', isRequired: false }, { name: 'skip', isRequired: false }]
+}));
 
 const MANIFEST = {
-  name: 'Porn',
+  name: 'Porn Plus',
   id: ID,
   version: _package.default.version,
-  description: `\
-Time to unsheathe your sword! \
-Watch porn videos and webcam streams from ${availableSites}\
-`,
-  types: ['movie', 'tv'],
+  description: 'Nuvio & Stremio Compatible Adult Addon',
+  types: ['movie'],
   idProperty: _PornClient.default.ID,
-  dontAnnounce: !IS_PROD,
-  sorts: _PornClient.default.SORTS,
-  // The docs mention `contactEmail`, but the template uses `email`
-  email: EMAIL,
-  contactEmail: EMAIL,
   endpoint: `${ENDPOINT}/stremioget/stremio/v1`,
   logo: `${ENDPOINT}/logo.png`,
   icon: `${ENDPOINT}/logo.png`,
   background: `${ENDPOINT}/bg.jpg`,
-  // OBSOLETE: used in pre-4.0 stremio instead of idProperty/types
+  // Nuvio'nun kategorileri görebilmesi için burası kritik:
+  catalogs: CATALOGS,
+  resources: ['catalog', 'meta', 'stream'],
+  // Eski tip Nuvio/CloudStream versiyonları için filtre:
   filter: {
-    [`query.${_PornClient.default.ID}`]: {
-      $exists: true
-    },
-    'query.type': {
-      $in: ['movie', 'tv']
-    }
+    [`query.${_PornClient.default.ID}`]: { $exists: true },
+    'query.type': { $in: ['movie'] }
   }
 };
 
+// Katalogdan veri çekme fonksiyonu
+function makeCatalogMethod(client) {
+  return function () {
+    var _refCatalog = _asyncToGenerator(function* (request, cb) {
+      try {
+        // catalog_ph -> ph (PornHub ID'sine çevir)
+        const adapterId = request.id.replace('catalog_', '');
+        const response = yield client.invokeMethod('meta.search', {
+          query: {
+            type: 'movie',
+            [_PornClient.default.ID]: adapterId,
+            search: (request.extra && request.extra.search) ? request.extra.search : ''
+          },
+          skip: (request.extra && request.extra.skip) ? request.extra.skip : 0
+        });
+        cb(null, response);
+      } catch (err) {
+        cb(err);
+      }
+    });
+    return function (req, callback) { return _refCatalog.apply(this, arguments); };
+  }();
+}
+
 function makeMethod(client, methodName) {
-  return (
-    /*#__PURE__*/
-    function () {
-      var _ref = _asyncToGenerator(function* (request, cb) {
-        let response;
-        let error;
-
-        try {
-          response = yield client.invokeMethod(methodName, request);
-        } catch (err) {
-          error = err;
-          /* eslint-disable no-console */
-
-          console.error( // eslint-disable-next-line prefer-template
-          _chalk.default.gray(new Date().toLocaleString()) + ' An error has occurred while processing ' + `the following request to ${methodName}:`);
-          console.error(request);
-          console.error(err);
-          /* eslint-enable no-console */
-        }
-
-        cb(error, response);
-      });
-
-      return function (_x, _x2) {
-        return _ref.apply(this, arguments);
-      };
-    }()
-  );
+  return function () {
+    var _ref = _asyncToGenerator(function* (request, cb) {
+      let response;
+      try {
+        response = yield client.invokeMethod(methodName, request);
+      } catch (err) {
+        console.error(`Error in ${methodName}:`, err);
+      }
+      cb(null, response);
+    });
+    return function (_x, _x2) { return _ref.apply(this, arguments); };
+  }();
 }
 
 function makeMethods(client, methodNames) {
-  return methodNames.reduce((methods, methodName) => {
-    methods[methodName] = makeMethod(client, methodName);
+  const baseMethods = methodNames.reduce((methods, methodName) => {
+    if (methodName !== 'catalog.find') {
+      methods[methodName] = makeMethod(client, methodName);
+    }
     return methods;
   }, {});
+  baseMethods['catalog.find'] = makeCatalogMethod(client);
+  return baseMethods;
 }
 
-let client = new _PornClient.default({
-  proxy: PROXY,
-  cache: CACHE
-});
+let client = new _PornClient.default({ proxy: PROXY, cache: CACHE });
 let methods = makeMethods(client, SUPPORTED_METHODS);
 let addon = new _stremioAddons.default.Server(methods, MANIFEST);
 
 let server = _http.default.createServer((req, res) => {
-  (0, _serveStatic.default)(STATIC_DIR)(req, res, () => {
+  (0, _serveStatic.default)('static')(req, res, () => {
     addon.middleware(req, res, () => res.end());
   });
 });
 
 server.on('listening', () => {
-  let values = {
-    endpoint: _chalk.default.green(MANIFEST.endpoint),
-    id: ID === DEFAULT_ID ? _chalk.default.red(ID) : _chalk.default.green(ID),
-    email: EMAIL ? _chalk.default.green(EMAIL) : _chalk.default.red('undefined'),
-    env: IS_PROD ? _chalk.default.green('production') : _chalk.default.green('development'),
-    proxy: PROXY ? _chalk.default.green(PROXY) : _chalk.default.red('off'),
-    cache: CACHE === '0' ? _chalk.default.red('off') : _chalk.default.green(CACHE === '1' ? 'on' : CACHE) // eslint-disable-next-line no-console
-
-  };
-  console.log(`
-    ${MANIFEST.name} Addon is listening on port ${PORT}
-
-    Endpoint:    ${values.endpoint}
-    Addon Id:    ${values.id}
-    Email:       ${values.email}
-    Environment: ${values.env}
-    Proxy:       ${values.proxy}
-    Cache:       ${values.cache}
-    `);
+  console.log(_chalk.default.green(`\n ✅ %100 Uyumlu Eklenti Hazır! \n Port: ${PORT}`));
 }).listen(PORT);
+
 var _default = server;
 exports.default = _default;
-//# sourceMappingURL=index.js.map
